d09aac395342b1dac34116a75662e06ff4d48eb3
taiki yamamoto <ultra0115.20@gmail.com>
2023-10-10 21:10:34 +0900

    詳細画面の金額の自動計算実装

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -11,4 +11,30 @@ class Invoice < ApplicationRecord
   validates :due_on, presence: true
   validates :api_status, presence: true
   enum api_status: { 未連携: 0, 完了: 1 }
+
+  def subtotal_price_without_tax
+    subtotal = 0
+    self.invoice_details.each do |invoice_detail|
+      invoice_detail.subject
+      quantity = invoice_detail.quantity
+      price = invoice_detail.unit_price
+      sum = quantity * price
+      subtotal += sum
+    end
+    subtotal
+  end
+
+  def total_price_with_tax
+    total = 0
+    self.invoice_details.each do |invoice_detail|
+      invoice_detail.subject
+      quantity = invoice_detail.quantity
+      price = invoice_detail.unit_price
+      subtotal = quantity * price
+      total += subtotal
+      total
+    end
+    total = (total * 1.10).floor
+  end
+
 end
